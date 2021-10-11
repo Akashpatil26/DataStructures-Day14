794e401e3eca0f05df6daa11d4a78efdd26baa5b
Akash Patil <akashpatilsrt@gmail.com>
2021-10-11 20:46:40 +0530

    Added UC4

--- src/javapractice/MyLinkedListTest.java
+++ src/javapractice/MyLinkedListTest.java
@@ -33,9 +33,19 @@ public class MyLinkedListTest {
                 myLinkedList.head.getNext().equals(mySecondNode) &&
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
-
-
-
+    }
+    public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult(){
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        boolean result=myLinkedList.head.equals(myFirstNode)&&
+                myLinkedList.head.getNext().equals(mySecondNode)&&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
 
     }
 }
